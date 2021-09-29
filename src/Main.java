b6b2c7849cc21b4ecb89c602d79f0161e6e2856d
Gaston Jolinont <falsee63@gmail.com>
2021-09-29 18:27:21 -0300

    Conociendo tipods de datos primitivos - JAVA -

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //DATOS DE TIPO PRIMITIVO EN JAVA
+
+        //variables Enteros
+
+        byte entero = 10;
+        short enter = 123;
+        int ente = 123456;
+        long ent = 123456789;
+
+        //Variables Decimales
+
+        float decimal = 3.64f; // se coloca una f al final del decimal
+        double decima = 3.4158745; // en double no hace falta incorporar la f.
+
+        //Variables Caracter
+
+        char caracter = 'a';
+
+        //Variables Booleanos
+
+        boolean decision = true;
+        boolean decisio = false;
+
+
+        System.out.println("El Entero byte vale: "+entero);
+        System.out.println("El Entero short vale: "+enter);
+        System.out.println("El Entero int vale: "+ente);
+        System.out.println("El Entero long vale: "+ent);
+        System.out.println();
+        System.out.println("El decimal float vale: "+decimal);
+        System.out.println("El decimal double vale: "+decima);
+        System.out.println();
+        System.out.println("En este caso la variable caracter vale: "+caracter);
+        System.out.println();
+        System.out.println("El booleano seria: "+decision);
+        System.out.println("El otro booleano seria: "+decisio);
+
+
+    }
+}
